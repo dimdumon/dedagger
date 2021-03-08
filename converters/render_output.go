781c3813d9c84182f7dae3eb62f7444a95e4963d
Mike Zak <feanorr@gmail.com>
2021-03-08 10:01:22 +0200

    Add converters for blocks and blockHeaders

--- converters/render_output.go
+++ converters/render_output.go
@@ -3,18 +3,72 @@ package converters
 import (
 	"encoding/json"
 
+	"github.com/kaspanet/kaspad/domain/consensus/utils/hashes"
+
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 )
+
+func jsonMarshal(output interface{}) (string, error) {
+	bytes, err := json.MarshalIndent(output, "", "\t")
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
 
 func RenderOutput(output interface{}) (string, error) {
 	switch outputObj := output.(type) {
 	case *externalapi.DomainHash:
 		return outputObj.String(), nil
+	case externalapi.BlockHeader:
+		return renderBlockHeader(outputObj)
+	case *externalapi.DomainBlock:
+		return renderBlock(outputObj)
 	default:
-		bytes, err := json.MarshalIndent(output, "", "\t")
-		if err != nil {
-			return "", err
-		}
-		return string(bytes), nil
+		return jsonMarshal(output)
+	}
+}
+
+func renderBlock(block *externalapi.DomainBlock) (string, error) {
+	jsonable := jsonableBlock(block)
+
+	return jsonMarshal(jsonable)
+}
+
+func jsonableBlock(block *externalapi.DomainBlock) interface{} {
+	return struct {
+		Header       interface{}
+		Transactions []*externalapi.DomainTransaction
+	}{
+		Header:       jsonableBlockHeader(block.Header),
+		Transactions: block.Transactions,
+	}
+}
+
+func renderBlockHeader(blockHeader externalapi.BlockHeader) (string, error) {
+	jsonable := jsonableBlockHeader(blockHeader)
+
+	return jsonMarshal(jsonable)
+}
+
+func jsonableBlockHeader(blockHeader externalapi.BlockHeader) interface{} {
+	return struct {
+		Version              uint16
+		ParentHashes         []string
+		HashMerkleRoot       string
+		AcceptedIDMerkleRoot string
+		UTXOCommitment       string
+		TimeInMilliseconds   int64
+		Bits                 uint32
+		Nonce                uint64
+	}{
+		Version:              blockHeader.Version(),
+		ParentHashes:         hashes.ToStrings(blockHeader.ParentHashes()),
+		HashMerkleRoot:       blockHeader.HashMerkleRoot().String(),
+		AcceptedIDMerkleRoot: blockHeader.AcceptedIDMerkleRoot().String(),
+		UTXOCommitment:       blockHeader.UTXOCommitment().String(),
+		TimeInMilliseconds:   blockHeader.TimeInMilliseconds(),
+		Bits:                 blockHeader.Bits(),
+		Nonce:                blockHeader.Nonce(),
 	}
 }
