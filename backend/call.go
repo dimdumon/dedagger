93be62a01ace291144f6d76300079e8ddfe7abc5
Mike Zak <feanorr@gmail.com>
2021-03-30 09:22:39 +0300

    Add stagingArea

--- backend/call.go
+++ backend/call.go
@@ -3,14 +3,25 @@ package backend
 import (
 	"reflect"
 
+	consensusmodel "github.com/kaspanet/kaspad/domain/consensus/model"
+
 	"github.com/svarogg/dedagger/model"
 )
 
 func (be *Backend) Call(method *model.Method, parameters []reflect.Value) []reflect.Value {
 	in := []reflect.Value{}
+	stagingAreaIndex := 0
 	if method.Value.Type().NumIn() > 0 && method.Value.Type().In(0).String() == "model.DBReader" {
 		valueOfDatabaseContext := reflect.ValueOf(be.consensus.DatabaseContext())
 		in = append(in, valueOfDatabaseContext)
+
+		stagingAreaIndex++
+	}
+
+	if method.Value.Type().NumIn() > stagingAreaIndex && method.Value.Type().In(stagingAreaIndex).String() == "*model.StagingArea" {
+		stagingArea := consensusmodel.NewStagingArea()
+		valueOfStagingArea := reflect.ValueOf(stagingArea)
+		in = append(in, valueOfStagingArea)
 	}
 	in = append(in, parameters...)
 	return method.Value.Call(in)
